8d164b9f1589f090272488bddd13eb14688c216b
Natalia Boudard <natalia.boudard@gmail.com>
2024-02-29 17:11:12 +0100

    A corriger! le gitgnore

--- Express2000/app.js
+++ Express2000/app.js
@@ -1,15 +1,25 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 
 // dossier publique
 app.use(express.static("public"));
 // Importez le routeur
 
-
-const mainRouter = require("./router/routes")
-
+const mainRouter = require("./router/routes");
 // Utilisez le routeur pour gérer les routes
 app.use('/', mainRouter);
+
+
+
+
+//------------CONNECTION MONGOOSE----------------
+mongoose.connect('mongodb://localhost:27017/user')
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'Erreur de connexion à MongoBD'));
+db.once('open', () => {
+  console.log("Connecté à MongoBD");
+})
 
 
 // port d'ecoute
