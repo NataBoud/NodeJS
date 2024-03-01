4fe9a1431abef2642c49a1c34e735818dbbca83f
Natalia Boudard <natalia.boudard@gmail.com>
2024-03-01 13:14:18 +0100

    FilmsApp / express & mongoose

---
+++ FilmsApp/src/routes/movieRoutes.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+const movieController = require("../controller/movieController");
+
+
+router.post("/add-movie", movieController.addMovie);
+router.get("/:title", movieController.getMovie);
+router.get("/movies", movieController.getAllMovies);
+router.put("/update-movie/:id", movieController.updateMovie);
+router.delete("/delete-movie/:id", movieController.deleteMovie);
+router.get("*", movieController.parDefautPage);
+
+module.exports = router;
